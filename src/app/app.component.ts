931b6c83edbd5cabbdef119dc296dbe1d105e834
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2024-07-17 10:06:29 +0100

    I completed the Navbar section design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { BodySectionComponent } from "./body-section/body-section.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, BodySectionComponent]
+    imports: [CommonModule, RouterOutlet, BodySectionComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'Soccer';
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "color-primary": "F8F0E3",
+        "color-secondary": "#FFA500",
+        "color-white": "#fff",
+        "color-black": "#000",
+        "color-ashe": "#B2BEB5",
+        "color-grey": "#808080",
+        "color-blue": "#0000FF",
+
+      }
+    },
+    container:{
+      center: true,
+      padding:{
+        DEFAULT:'10px',
+        md:"50px"
+      }
+    }
   },
   plugins: [],
 }
