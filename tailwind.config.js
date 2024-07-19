10a1e11caa196935d207c8e743e7b0e413c87a6f
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2024-07-19 12:38:19 +0100

    Began the body section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+      backgroundImage: {
+        'custom-image': "url('assets/soccer-imagee.png')",
+      },
       colors:{
         "color-primary": "F8F0E3",
         "color-secondary": "#FFA500",
@@ -13,6 +17,7 @@ module.exports = {
         "color-ashe": "#B2BEB5",
         "color-grey": "#808080",
         "color-blue": "#0000FF",
+        "color-red": "#Ff0000"
 
       }
     },
